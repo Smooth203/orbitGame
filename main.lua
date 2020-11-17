94a4514e073a3500dcc6d50cb5f7a9e14caf8b78
James <jamesjwaggett@gmail.com>
2020-11-17 20:57:22 +0000

    small

--- main.lua
+++ main.lua
@@ -2,12 +2,15 @@ io.stdout:setvbuf("no") -- live console for ST3
 
 function newObject(name, parent, semiMajorAxis, eccentricity)
 	object = {}
-	object.body = love.physics.newBody(world, (0.63781/2), (0.63781/2), 'dynamic')
-	object.shape = love.physics.newCircleShape(0.63781)
+	object.body = love.physics.newBody(world, 0, 0, 'dynamic')
+	object.shape = love.physics.newCircleShape(100)
 	object.fixture = love.physics.newFixture(object.body, object.shape)
 	object.fixture:setDensity(1)
 	_, _, mass, _ = object.shape:computeMass(1)
 	object.body:setMass(mass)
+
+	print(parent.body:getY()+(semiMajorAxis-eccentricity))
+
 	object.body:setPosition(parent.body:getX(), parent.body:getY()+(semiMajorAxis-eccentricity))
 
 	objects[name] = object
@@ -18,7 +21,7 @@ function love.load()
 	world = love.physics.newWorld(0, 0, true)
 
 	--gravity
-	G = 1000
+	G = 1
 
 
 	objects = {}
@@ -29,8 +32,8 @@ function love.load()
 	objects.sun = {}
 
 	-- x/2 because body anchors from centre so 5000 in each direction, total 1000
-	objects.sun.body = love.physics.newBody(world, (69.634/2), (69.634/2), 'dynamic')
-	objects.sun.shape = love.physics.newCircleShape(69.634)
+	objects.sun.body = love.physics.newBody(world, 0, 0, 'dynamic')
+	objects.sun.shape = love.physics.newCircleShape(10920)
 	-- attach shape to body
 	objects.sun.fixture = love.physics.newFixture(objects.sun.body, objects.sun.shape)
 	objects.sun.fixture:setDensity(0.255)
@@ -38,14 +41,11 @@ function love.load()
 	objects.sun.body:setMass(mass)
 	objects.sun.body:setPosition(0, 0)
 
-	newObject('planet', objects.sun, 15210 , 0)
+	GG = (29780^2)/(25000 * objects.sun.body:getMass())
+	print(GG)
+	newObject('planet', objects.sun, 25000 , 0)
 
-	mu = G * objects.sun.body:getMass()
-	
-	vv = math.sqrt(mu * ( (2/love.physics.getDistance(objects.planet.fixture, objects.sun.fixture))-(1/love.physics.getDistance(objects.planet.fixture, objects.sun.fixture)) ))
-	print(vv)
-
-	objects.planet.body:setLinearVelocity(vv, 0)
+	objects.planet.body:setLinearVelocity(400,0)
 	--objects.planet.body:applyLinearImpulse(5000, 0)
 	--world:translateOrigin(-(love.graphics.getWidth()/2)/scale, (objects.planet.body:getY())-(love.graphics.getHeight()/2)/scale)
 	world:translateOrigin(-(love.graphics.getWidth()/2)/scale,-(love.graphics.getHeight()/2)/scale)
@@ -56,24 +56,13 @@ function love.update(dt)
 
 	--gravity
 
-	dist = love.physics.getDistance(objects.planet.fixture, objects.sun.fixture)
+	dist = math.sqrt(((objects.planet.body:getX()-objects.sun.body:getX())^2)+((objects.planet.body:getY()-objects.sun.body:getY())^2))
 	angle = math.atan2( objects.sun.body:getY()-objects.planet.body:getY(), objects.sun.body:getX()-objects.planet.body:getX() )
 	force = (G * objects.sun.body:getMass() * objects.planet.body:getMass()) / (dist)
 	objects.planet.body:applyForce(force*math.cos(angle), force*math.sin(angle))
 
-
-
-	--print(objects.sun.body:getMass(), objects.sun.shape:getRadius())
-	--print(objects.sun.body:getMass())
-	--print(dist, objects.planet.body:getLinearVelocity())
-
-	--tv = objects.planet.body:getAngularVelocity() * dist -- vel tangential to motion
-	--print(tv)
-
 	vx, vy = objects.planet.body:getLinearVelocity()
 	v = math.sqrt(vx^2 + vy^2)
-	--print(vx, vy)
-	--world:translateOrigin(vx*dt, vy*dt)
 
 	if love.keyboard.isDown('up') then
 		world:translateOrigin(0, -10/scale)
@@ -95,7 +84,6 @@ function love.wheelmoved(x,y)
 		scale = scale - 0.001
 		--world:translateOrigin()
 	end
-	print(scale, objects.planet.body:getWorldPoint(objects.planet.body:getX(),objects.planet.body:getY()))
 end
 
 function love.draw()
@@ -107,7 +95,7 @@ function love.draw()
 
 	love.graphics.setColor(1,1,1,1)
 
-	love.graphics.ellipse('line', (love.graphics.getWidth()/2),(love.graphics.getHeight()/2), 15210*scale, 15210*scale)
+	love.graphics.ellipse('line', (love.graphics.getWidth()/2),(love.graphics.getHeight()/2), 25000*scale, 25000*scale)
 	love.graphics.line((love.graphics.getWidth()/2),(love.graphics.getHeight()/2), objects.planet.body:getX()*scale, objects.planet.body:getY()*scale)
 
 	love.graphics.print( "Mass:"..objects.planet.body:getMass().. "kg,\nAltitude: "..math.floor(dist).."m,\n".."Velocity: "..math.floor(v)..'m/s,\n'.."Grav. Force: "..(force).."N", objects.planet.body:getX()*scale, objects.planet.body:getY()*scale)
